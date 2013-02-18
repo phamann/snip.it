e5ac0b7cf99d6829c904fd6a5fbab381217caa7b
Patrick Hamann <patrick.hamann@guardian.co.uk>
2013-02-18 14:21:18 +0000

    basic hightlighting functionality

--- snipit.js
+++ snipit.js
@@ -1,14 +1,34 @@
 //Global
 var Snipit = Snipit || {};
 
+Snipit.config = {
+    isActive: false,
+    isWrapped: false
+};
+
 Snipit.init = function() {
     Snipit.showCta();
-    Snipit.highlighter.init();
 };
 
 Snipit.showCta = function() {
-    var tpl = '<li class="full-line snipit"><a clas="snipit-cta">Snip.it</a></li>';
-    $('.share-links.floating').append(tpl);
+    var tpl = '<li class="full-line snipit-cta"><button class="snipit-cta-btn">Snipit</button></li>';
+    $('#article-toolbox-side').prepend(tpl);
+
+    $('.snipit-cta-btn').on('click', function(e) {
+        e.preventDefault();
+
+        if(!Snipit.config.isWrapped) {
+            Snipit.highlighter.init();
+        }
+
+        if(Snipit.config.isActive) {
+            Snipit.highlighter.unbind();
+            Snipit.config.isActive = false;
+        } else {
+            Snipit.highlighter.bind();
+            Snipit.config.isActive = true;
+        }
+    });
 };
 
 //Kick it all of on domready
