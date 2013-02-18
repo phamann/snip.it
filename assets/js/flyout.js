d0aa05c41c9c5d1a9e31a00f933a489ee4d57022
James Gorrie <james.gorrie@guardian.co.uk>
2013-02-18 13:27:23 +0000

    Events put on slyout

--- assets/js/flyout.js
+++ assets/js/flyout.js
@@ -1,15 +1,20 @@
 /*
  * To use do something like the follwing:
- 	snipit.flyout.open({
+ 	Snipit.flyout.open({
 		article_link: 'http://www.guardian.co.uk/artanddesign/interactive/2013/feb/01/view-from-top-shard-london-interactive',
 		article_headline: 'The view from the top of the Shard: London panorama of sights and sounds – interactive',
 		selected_content: 'Read Antony Gormley, Tony Benn, Diana Athill and other famous Londoners on their favourite places'
-	}, position: { top: 20, left: 50 });
+	}, { top: 20, left: 50 });
  * 
  */
 var Snipit = Snipit || {};
 Snipit.flyout = (function() {
+	function init() {
+		console.log('Snipit.flyout.init');
+	}
+
 	function open(content, position) {
+		console.log('Snipit.flyout.open');
 		var url = chrome.extension.getURL('flyout.html'),
 			contentBit,
 			snipitBox;
@@ -22,12 +27,21 @@ Snipit.flyout = (function() {
 				}
 
 				snipitBox = $(html);
-				html.css({ top: position.top, left: position.left });
-				$('body').append(html);
+				snipitBox.css({ top: position.top, left: position.left });
+				$('body').append(snipitBox);
+
+				// TODO this definitely shoudn't be done like this
+				snipitBox.on('click', '[data-snipit-action]', function() {
+					var el = $(this),
+						action = el.attr('data-snipit-action');
+
+					console.log(action);
+				});
 			});
 	}
 
 	return {
+		init: init,
 		open: open
 	};
 })();
--- snipit.js
+++ snipit.js
@@ -4,6 +4,13 @@ var Snipit = Snipit || {};
 Snipit.init = function() {
     Snipit.showCta();
     Snipit.highlighter.init();
+	Snipit.flyout.init();
+
+    Snipit.flyout.open({
+		article_link: 'http://www.guardian.co.uk/artanddesign/interactive/2013/feb/01/view-from-top-shard-london-interactive',
+		article_headline: 'The view from the top of the Shard: London panorama of sights and sounds – interactive',
+		selected_content: 'Read Antony Gormley, Tony Benn, Diana Athill and other famous Londoners on their favourite places'
+	}, { top: 20, left: 50 });
 };
 
 Snipit.showCta = function() {
