6df33ba297406811a091e97747244b33b77bf075
James Gorrie <james.gorrie@guardian.co.uk>
2013-02-18 12:54:10 +0000

    Basic styling added to flyout and flyout.open added

--- assets/js/flyout.js
+++ assets/js/flyout.js
@@ -1,7 +1,18 @@
 var snipit = snipit || {};
 snipit.flyout = (function() {
 	function open(content) {
-		console.log(content);
+		var url = chrome.extension.getURL('flyout.html'),
+			content_bit;
+
+		$.get(url)
+			.then(function(html) {
+				for (var i in content) {
+					content_bit = content[i];
+					html = html.replace('{{ ' + i + ' }}', content_bit);
+				}
+
+				$('body').append(html);
+			});
 	}
 
 	return {
--- snipit.js
+++ snipit.js
@@ -1,2 +1,5 @@
-alert('test');
-snipit.flyout.open('CONTENT');
+snipit.flyout.open({
+	article_link: 'http://www.guardian.co.uk/artanddesign/interactive/2013/feb/01/view-from-top-shard-london-interactive',
+	article_headline: 'The view from the top of the Shard: London panorama of sights and sounds – interactive',
+	selected_content: 'Read Antony Gormley, Tony Benn, Diana Athill and other famous Londoners on their favourite places'
+});
