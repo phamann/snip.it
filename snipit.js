3c0c5d191adce10dd8374b84396e301f9f336a15
Patrick Hamann <patrick.hamann@guardian.co.uk>
2013-02-18 12:26:52 +0000

    Setup init functions

--- snipit.js
+++ snipit.js
@@ -1,1 +1,22 @@
-alert('test');
+SnipIt = {};
+
+SnipIt.init = function() {
+    this.showCta();
+    this.bind();
+};
+
+SnipIt.showCta = function() {
+    var tpl = '<li class="full-line snipit"><a clas="snipit-cta">Snip.it</a></li>';
+    $('.share-links').append(tpl);
+};
+
+SnipIt.bind = function() {
+
+};
+
+$(document).ready(function() {
+    var isArticle = ($('#article-wrapper').length > 0);
+    if(isArticle) {
+        SnipIt.init();
+    }
+});
